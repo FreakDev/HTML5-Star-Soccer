697ed6adcfeed80535cb45fcf748d9c90dc83433
Mathias Desloges <freakdev@gmail.com>
2016-07-05 12:44:17 +0200

    poc with phaser

---
+++ game.js
@@ -0,0 +1,179 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var world, game = new Phaser.Game(640, 480, Phaser.AUTO, "c", { preload: preload, create: create, update: update });
+
+  var p, b;
+  var mouseX, mouseY, mousePVec, isMouseDown, selectedBody, mouseJoint;
+  var canvasPosition;
+  var b2Vec2 = Box2D.Common.Math.b2Vec2,
+      b2Body = Box2D.Dynamics.b2Body,
+      b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
+      b2World = Box2D.Dynamics.b2World,
+      b2AABB = Box2D.Collision.b2AABB;
+  var debugDraw;
+
+  function getBodyAtMouse() {
+      mousePVec = new b2Vec2(mouseX, mouseY);
+      var aabb = new b2AABB();
+      console.log(mouseX, mouseY);
+      aabb.lowerBound.Set(mouseX - 1000, mouseY - 1000);
+      aabb.upperBound.Set(mouseX + 1000, mouseY + 1000);
+
+      // Query the world for overlapping shapes.
+
+      selectedBody = null;
+      world.QueryAABB(getBodyCB, aabb);
+      console.log(selectedBody);
+      return selectedBody;
+  }
+
+  function getBodyCB(fixture) {
+      if(fixture.GetBody().GetType() != b2Body.b2_staticBody) {
+          if(fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), mousePVec)) {
+              selectedBody = fixture.GetBody();
+              return false;
+          }
+      }
+      return true;
+  }
+
+
+  function preload() {
+      game.load.image('ground', 'assets/background.png');
+
+      // ball
+      game.load.image('ball', 'assets/ball.png');
+
+      // player
+      game.load.image('red-player', 'assets/red-player.png');
+  }
+
+  function create() {
+
+      world = new b2World(
+          new b2Vec2(0, 0)    // 0 gravity
+          ,  true             // allow sleep
+      );        
+
+      canvasPosition = getElementPosition(document.querySelector("canvas"));
+
+      document.addEventListener("mousedown", function(e) {
+          isMouseDown = true;
+          handleMouseMove(e);
+          document.addEventListener("mousemove", handleMouseMove, true);
+      }, true);
+
+      document.addEventListener("mouseup", function() {
+          document.removeEventListener("mousemove", handleMouseMove, true);
+          isMouseDown = false;
+      }, true);
+
+      function handleMouseMove(e) {
+          mouseX = (e.clientX - canvasPosition.x) / 30;
+          mouseY = (e.clientY - canvasPosition.y) / 30;
+      };
+
+      game.add.sprite(0, 0, 'ground');
+
+      p = new Circle(game, game.world.centerX - 50, game.world.centerY - 50, 'red-player', 0.15, world, 0.6, 0.8, 0.6);
+  /*    var p = game.add.sprite(game.world.centerX - 50, game.world.centerY - 50, 'red-player');
+      ball.anchor.setTo(0.5, 0.5);
+      ball.scale.set(0.2, 0.2);
+  */
+
+      b = new Circle(game, game.world.centerX, game.world.centerY, 'ball', 0.5, world, 0.4, 0.2, 0.4);
+  /*    var ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
+      ball.anchor.setTo(0.5, 0.5);
+      ball.scale.set(0.5, 0.5);    
+  */
+
+      debugDraw = new b2DebugDraw();
+      debugDraw.SetSprite(document.querySelector('#debug').getContext('2d'));
+      debugDraw.SetDrawScale(30.0);
+      debugDraw.SetFillAlpha(0.5);
+      debugDraw.SetLineThickness(1.0);
+      debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+      world.SetDebugDraw(debugDraw);
+  }
+
+  function update() {
+
+      // var ctxt = game.context;
+
+      if(isMouseDown && (!mouseJoint)) {
+         var body = getBodyAtMouse();
+         if(body) {
+            // var md = new b2MouseJointDef();
+            // md.bodyA = world.GetGroundBody();
+            // md.bodyB = body;
+            // md.target.Set(mouseX, mouseY);
+            // md.collideConnected = true;
+            // md.maxForce = 300.0 * body.GetMass();
+            mouseJoint = true;
+            // mouseJoint = world.CreateJoint(md);
+         }
+      }
+      
+      if(mouseJoint) {
+         if(isMouseDown) {
+            // mouseJoint.SetTarget(new b2Vec2(mouseX, mouseY));
+         } else {
+            if (selectedBody) {
+               var bodyCenter = selectedBody.GetWorldCenter();
+               selectedBody.ApplyImpulse(new b2Vec2((bodyCenter.x - mouseX) * 2, (bodyCenter.y - mouseY) * 2), bodyCenter)
+               mouseX = undefined;
+               mouseY = undefined;
+               // world.DestroyJoint(mouseJoint);
+               mouseJoint = false;                     
+            }
+         }
+      }
+   
+      // check if anybody is awake
+  /*    var hasAwake = false;
+      bodies.forEach(function (body) {
+
+         if (body.IsAwake()) {
+            hasAwake = true;
+         }
+  */    //    if (bodyV.x != 0 || bodyV.y != 0) {
+      //       targetX = Math.abs(bodyV.x) < 0.1 ? 0 : bodyV.x * 0.995;
+      //       targetY = Math.abs(bodyV.y) < 0.1 ? 0 : bodyV.y * 0.995;
+      //       console.log("decrease velocity",bodyV, {x:targetX, y:targetY});
+      //       body.SetLinearVelocity({x:targetX, y:targetY});
+      //    }
+  /*    });
+      console.log(hasAwake ? "wait for it" : "play");
+  */
+
+      world.Step(1 / 60, 10, 10);
+
+      p.update();
+      b.update();
+
+      world.DrawDebugData();
+      world.ClearForces();    
+  }
+
+   //http://js-tut.aardon.de/js-tut/tutorial/position.html
+   function getElementPosition(element) {
+      var elem=element, tagname="", x=0, y=0;
+     
+      while((typeof(elem) == "object") && (typeof(elem.tagName) != "undefined")) {
+         y += elem.offsetTop;
+         x += elem.offsetLeft;
+         tagname = elem.tagName.toUpperCase();
+
+         if(tagname == "BODY")
+            elem=0;
+
+         if(typeof(elem) == "object") {
+            if(typeof(elem.offsetParent) == "object")
+               elem = elem.offsetParent;
+         }
+      }
+
+      return {x: x, y: y};
+   }
+
+});
